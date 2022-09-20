1ccbd1d81f216deb51cc3894ef6b911d6f6dec3b
Wator Vapor <watovapor@gmail.com>
2022-09-20 13:34:49 +0000

    did

--- static-www/www/assets/js/did/document.js
+++ static-www/www/assets/js/did/document.js
@@ -1,27 +1,14 @@
-import * as MASS from './gravity/mass.js';
-export class DIDAuth {
-  static DIDAuth = true;
-  constructor() {
-    if(DIDAuth.debug) {
-      console.log('DIDAuth::constructor:MASS=<',MASS,'>');
+import * as MASS from '../gravity/mass.js';
+export class DIDDocument {
+  static debug = true;
+  constructor(prefix) {
+    if(DIDDocument.debug) {
+      console.log('DIDDocument::constructor:MASS=<',MASS,'>');
     }
-    this.mass_ = new MASS.Mass(constDIDAuthPrefix);
-    if(DIDAuth.debug) {
-      console.log('DIDAuth::constructor:this.mass_=<',this.mass_,'>');
+    this.mass_ = new MASS.Mass(prefix);
+    if(DIDDocument.debug) {
+      console.log('DIDDocument::constructor:this.mass_=<',this.mass_,'>');
     }
-    DIDAuth.name_ = localStorage.getItem(constEdAuthName);
-  }
-  pub() {
-    return this.mass_.pubKeyB64_;
-  }
-  secret() {
-    return this.mass_.priKeyB64_;
-  }
-  address() {
-    return 'did:maap:' + this.mass_.address_;
-  }
-  name() {
-    return DIDAuth.name_;
   }
   document() {
     const didDoc = {
@@ -67,23 +54,11 @@ export class DIDAuth {
     didDoc.proof.push(proof);
     return didDoc;
   }
-  storeName(name) {
-    DIDAuth.name_ = name;
-    localStorage.setItem(constEdAuthName,name);
-  }
-  verifySecretKey(secretKey) {
-    if(DIDAuth.debug) {
-      console.log('DIDAuth::verifySecretKey:secretKey=<',secretKey,'>');
-    }
-    return this.mass_.verifySecretKey(secretKey);
-  }
+}
 
-  importSecretKey(secretKey) {
-    if(DIDAuth.debug) {
-      console.log('DIDAuth::importSecretKey:secretKey=<',secretKey,'>');
-    }
-    return this.mass_.importSecretKey(secretKey);
+export class DIDSeedDocument extends DIDDocument {
+  static debug = true;
+  constructor() {
+    super(constDIDAuthPrefix);
   }
-  static debug = false;
-  static name_ = null;
 }
