caf7173b5ceaa8f5d92b070fb0b6b43174ab9335
Wator Vapor <watovapor@gmail.com>
2022-09-26 03:18:36 +0000

    chain of evidence

--- static-www/www/account/about/about.js
+++ static-www/www/account/about/about.js
@@ -1,156 +1,210 @@
 document.addEventListener('DOMContentLoaded', async (evt) => {
   console.log('DOMContentLoaded::evt=<',evt,'>');
-  //createVisNetworkGraph_(evt);
   createD3Graphviz_(evt);
 });
 
+const createD3Graphviz_ = () => {
+  d3.select('#did-arch')
+  .graphviz()
+  .renderDot(gDidArchDOTString);
+  d3.select('#did-document')
+  .graphviz()
+  .renderDot(gDidDocumentDOTString);
+}
 
-const gDidMaapDOTstring = `
+const gDidArchDOTString = `
 digraph G {
-  label = "种子设备创建DID，追加设备都共同使用种子设备DID，每追加设备都对DID文档加上自己的签名";
+  label = "种子设备创建DID标识符和DID文档，
+  新追加设备都共同使用种子设备DID标识符，
+  每新追加设备都对DID文档加上自己的签名和已经追加设备的数字签名";
   rankdir = "LR";  
   subgraph cluster_A {
     A[shape=box,label="种子设备",fontname="bold",color="red"];
-    A001[shape=folder,label="种子设备 DID",color="red"];
+    A001[shape=folder,label="种子设备 DID标识符",color="red"];
     A003[shape=note,label="种子设备 公钥哈希",color="red"];
     A002[shape=note,label="种子设备 DID文档",color="red"];
     A004[shape=note,label="种子设备 密钥签名",color="red"];    
+    #A005[shape=note,label="种子设备 密钥 Web存储 | 本地存储",color="red"];    
 
     A -> A001[arrowhead=dot];
     A001 -> A002[arrowhead=dot];
     A001 -> A003[arrowhead=dot]
     A002 -> A004[arrowhead=dot]
+    #A004 -> A005[arrowhead=dot]
     label = "种子设备";
     color="red"
   }
 
   subgraph cluster_B {
     B[shape=box,label="追加设备1",fontname="bold",color="#008080"];
-    B001[shape=folder,label="种子设备 DID",color="red"];
+    B001[shape=folder,label="种子设备 DID标识符",color="red"];
     B003[shape=note,label="种子设备 公钥哈希",color="red"];
-    B002[shape=note,label="追加设备1 DID文档",color="#008080"];
+    B002[shape=note,label="追加设备1后 DID文档",color="#008080"];
     B004[shape=note,label="种子设备 密钥签名",color="red"];    
     B005[shape=note,label="追加设备1 密钥签名",color="#008080"];
+    
+    #B006[shape=note,label="追加设备1 密钥 Web存储 | 本地存储",color="#008080"];
     B -> B001[arrowhead=dot];
     B001 -> B002[arrowhead=dot];
     B001 -> B003[arrowhead=dot]
     B002 -> B004[arrowhead=dot]
+    B002 -> B005[arrowhead=dot]
+    #B005 -> B006[arrowhead=dot]
     label = "追加设备1";
     color="#008080"
   }
 
   subgraph cluster_C {
     C[shape=box,label="追加设备2",fontname="bold",color="#191970"];
-    C001[shape=folder,label="种子设备 DID",color="red"];
+    C001[shape=folder,label="种子设备 DID标识符",color="red"];
     C003[shape=note,label="种子设备 公钥哈希",color="red"];
-    C002[shape=note,label="追加设备2 DID文档",color="#191970"];
+    C002[shape=note,label="追加设备2后 DID文档",color="#191970"];
     C004[shape=note,label="种子设备 密钥签名",color="red"];    
     C005[shape=note,label="追加设备1 密钥签名",color="#008080"];
     C006[shape=note,label="追加设备2 密钥签名",color="#191970"];
+    #C007[shape=note,label="追加设备2 密钥 Web存储 | 本地存储",color="#008080"];
+    
     C -> C001[arrowhead=dot];
     C001 -> C002[arrowhead=dot];
     C001 -> C003[arrowhead=dot]
     C002 -> C004[arrowhead=dot]
+    C002 -> C005[arrowhead=dot]
+    C002 -> C006[arrowhead=dot]
+    #C006 -> C007[arrowhead=dot]
+    
     label = "追加设备2";
     color="#191970"
   }
 
   subgraph cluster_D {
     D[shape=box,label="追加设备...",fontname="bold",color="#663399"];
-    D001[shape=folder,label="种子设备 DID",color="red"];
+    D001[shape=folder,label="种子设备 DID标识符",color="red"];
     D003[shape=note,label="种子设备 公钥哈希",color="red"];
-    D002[shape=note,label="追加设备... DID文档",color="#663399"];
+    D002[shape=note,label="追加设备...后 DID文档",color="#663399"];
     D004[shape=note,label="种子设备 密钥签名",color="red"];    
     D005[shape=note,label="追加设备1 密钥签名",color="#008080"];
     D006[shape=note,label="追加设备2 密钥签名",color="#191970"];
     D007[shape=note,label="追加设备... 密钥签名",color="#663399"];
+    
+    #D008[shape=note,label="追加设备... 密钥 Web存储 | 本地存储",color="#663399"];
+    
     D -> D001[arrowhead=dot];
     D001 -> D002[arrowhead=dot];
     D001 -> D003[arrowhead=dot]
     D002 -> D004[arrowhead=dot]
+    D002 -> D005[arrowhead=dot]
+    D002 -> D006[arrowhead=dot]
+    D002 -> D007[arrowhead=dot]
+    
+    #D007 -> D008[arrowhead=dot]
+
     label = "追加设备...";
     color="#663399"
   }
   A -> B[];
   B -> C[];
   C -> D[]
+  
+  A002 -> B002[];
+  B002 -> C002[];
+  C002 -> D002[]
 
   
-  { rank=same; A A001 A002 }
-  { rank=same; B B001 B002 }
-  { rank=same; C C001 C002 }
-  { rank=same; D D001 D002 }
+  { rank=same; A A001 A002 A003 A004 }
+  { rank=same; B B001 B002 B003 B004 B005 }
+  { rank=same; C C001 C002 C003 C004 C005 C006 }
+  { rank=same; D D001 D002 D003 D004 D005 D006 D007}
 
 }`;
 
-const createD3Graphviz_ = () => {
-  d3.select('#did-arch')
-  .graphviz()
-  .renderDot(gDidMaapDOTstring);
-}
+
+const gDidDocumentDOTString = `
+digraph G {
+  label = "新设备加入已经存在的DID组织";
+  rankdir = "LR";  
+  subgraph cluster_A {
+    A[shape=box,label="要加入组织设备(起始状态)",fontname="bold",color="red"];
+    A001[shape=folder,label="种子设备 DID标识符",color="red"];
+    A003[shape=note,label="种子设备 公钥哈希",color="red"];
+
+    A -> A001[arrowhead=dot];
+    A001 -> A003[arrowhead=dot]
+    color="red"
+  }
+
+  subgraph cluster_B {
+    B[shape=box,label="要加入组织设备（游客身份）",fontname="bold",color="#008080"];
+    B001[shape=folder,label="种子设备 DID标识符",color="red"];
+    B003[shape=note,label="种子设备 公钥哈希",color="red"];
+    B002[shape=note,label="要加入组织设备 游客身份DID文档",color="#008080"];
+    B004[shape=note,label="要加入组织设备 密钥签名",color="#008080"];
+    B005[shape=note,label="要加入组织设备 成员身份DID文档",color="#008080"];
+    B -> B001[arrowhead=dot];
+    B001 -> B002[arrowhead=dot];
+    B001 -> B003[arrowhead=dot]
+    B002 -> B004[arrowhead=dot];
+    B002 -> C005[label="登入"];
+    B002 -> C002[label="发布"];
+    B002 -> B005[arrowhead=dot];
+    B005 -> B004[arrowhead=dot];
+
+    label = "要加入组织设备（游客身份）";
+    color="#008080"
+  }
+
+  subgraph cluster_C {
+    C[shape=box,label="MQTT Broker",fontname="bold",color="#191970"];
+    C001[shape=folder,label="成员主题(<DID标识符>/#)",color="#191970"];
+    C002[shape=folder,label="游客主题(<DID标识符>/guest/did/join/document)",color="#191970"];
+    C003[shape=folder,label="游客主题(<DID标识符>/guest/did/authed/document)",color="#191970"];
+    C004[shape=folder,label="JWT认证 成员DID文档",color="#191970"];
+    C005[shape=folder,label="JWT认证 游客DID文档",color="#191970"];
+    C -> C001[arrowhead=dot];
+    C -> C002[arrowhead=dot];
+    C -> C003[arrowhead=dot];
+    C -> C004[arrowhead=dot];
+    C -> C005[arrowhead=dot];
+    C002 -> D006[label="订阅"];
+    C003 -> B005[label="订阅"];
+    label = "MQTT Broker";
+    color="#191970"
+  }
 
 
-const createVisNetworkGraph_  = (evt) => {
-  //console.log('createVisNetworkGraph_::evt=<',evt,'>');
-  /*
-  const nodes = new vis.DataSet([
-    {id: 1, label: '种子设备1'},
-    {id: 1000, label: '种子设备 DID'},
-    {id: 1001, label: '种子设备 DID文档'},
 
-    {id: 2, label: '追加设备2'},
-    {id: 2000, label: '种子设备 DID'},
-    {id: 2001, label: '追加设备2 DID文档'},
-
-    {id: 3, label: '追加设备3'},
-    {id: 3000, label: '种子设备 DID'},
-    {id: 3001, label: '追加设备3 DID文档'},
+  subgraph cluster_D {
+    D[shape=box,label="组成员设备",fontname="bold",color="#663399"];
+    D001[shape=folder,label="种子设备 DID标识符",color="red"];
+    D003[shape=note,label="种子设备 公钥哈希",color="red"];
+    D002[shape=note,label="组成员设备 DID文档",color="#663399"];
+    D004[shape=note,label="种子设备 密钥签名",color="red"];    
+    D005[shape=note,label="组成员设备 密钥签名",color="#663399"];
+    D006[shape=note,label="组成员设备 加入新设备后DID文档",color="#663399"];
     
-    {id: 4, label: '追加设备...'},
-    {id: 4000, label: '种子设备... DID'},
-    {id: 4001, label: '追加设备... DID文档'},
-    
-    {id: 5, label: '追加设备n'},
-    {id: 5000, label: '种子设备 DID'},
-    {id: 5001, label: '追加设备n DID文档'},
+    D -> D001[arrowhead=dot];
+    D001 -> D002[arrowhead=dot];
+    D001 -> D003[arrowhead=dot]
+    D002 -> D004[arrowhead=dot]
+    D002 -> D005[arrowhead=dot]
+    D002 -> D006[arrowhead=dot,label="追加新设备公钥"]
+    D002 -> C004[label="登入"]
+    D006 -> D005[arrowhead=dot]
+    D006 -> C003[label="发布"];
+    label = "组成员设备";
+    color="#663399"
+  }
 
-  ]);
-  const edges = new vis.DataSet([
-    {from: 1, to: 2, arrows: 'to'},
-    {from: 1000, to: 1, arrows: 'to'},
-    {from: 1001, to: 1, arrows: 'to'},
+  
+  A -> B[];
+  B -> C[];
+  C -> D[];
+  A001-> B001[label="手动复制粘贴"];
 
-    {from: 2, to: 3, arrows: 'to'},
-    {from: 2000, to: 2, arrows: 'to'},
-    {from: 2001, to: 2, arrows: 'to'},
+  
+  { rank=same; A A001 A003 }
+  { rank=same; B B001 B002 B003 B004 B005 }
+  { rank=same; C C001 C002 C003 C004 C005 }
+  { rank=same; D D001 D002 D003 D004 D005 D006}
 
-    {from: 3, to: 4, arrows: 'to'},
-    {from: 3000, to: 3, arrows: 'to'},
-    {from: 3001, to: 3, arrows: 'to'},
+}`;
 
-    {from: 4, to: 5, arrows: 'to'},
-    {from: 4000, to: 4, arrows: 'to'},
-    {from: 4001, to: 4, arrows: 'to'},
-
-    {from: 5000, to: 5, arrows: 'to'},
-    {from: 5001, to: 5, arrows: 'to'},
-
-  ]);
-  const data = {
-    nodes: nodes,
-    edges: edges
-  };      
-  */
-  console.log('createVisNetworkGraph_::vis=<',vis,'>');
-  const parsedData = vis.parseDOTNetwork(gDidMaapDOTstring);
-  const data = {
-    nodes: parsedData.nodes,
-    edges: parsedData.edges
-  };      
-  console.log('createVisNetworkGraph_::data=<',data,'>');
-  const options = {};
-  const container = document.getElementById('did-arch');
-  console.log('createVisNetworkGraph_::container=<',container,'>');
-  const didArch = new vis.Network(container, data, options);
-  console.log('createVisNetworkGraph_::didArch=<',didArch,'>');
-}
